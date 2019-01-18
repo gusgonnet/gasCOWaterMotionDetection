2e1714bc1db07db4a01316bf967162f2fb3dcd2e
ceajog <gustavo.gonnet@ericsson.com>
2019-01-18 14:15:37 -0500

    adding prj link in hackster

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gasCOWaterMotionDetection
 Detect gas, carbon monoxide, motion and water leaks with this multipurpose project.
+
+Find the project description here:
+
+[https://www.hackster.io/gusgonnet/gas-carbon-monoxide-motion-and-water-detection-1f16e3](https://www.hackster.io/gusgonnet/gas-carbon-monoxide-motion-and-water-detection-1f16e3)
--- detector.ino
+++ detector.ino
@@ -17,8 +17,9 @@
 // You do not have to comply with the license for elements of the material in the public domain or where your use is permitted by an applicable exception or limitation.
 // No warranties are given. The license may not give you all of the permissions necessary for your intended use. For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.
 //
-// github: https://github.com/gusgonnet/XXXX
+// github: https://github.com/gusgonnet
 // hackster: https://www.hackster.io/gusgonnet
+// project: https://www.hackster.io/gusgonnet/gas-carbon-monoxide-motion-and-water-detection-1f16e3
 //
 // Free for personal use.
 //
